f75bc7c2cc8b19a01af22bcbcbdd77895c2751ad
Pawel <pw02.01@gmail.com>
2016-03-25 11:58:23 +0100

    dodanie listy tematów do DtoSection

--- ProjectBD/trainingCompany-repo/src/main/java/pl/trainingCompany/repo/TopicRepo.java
+++ ProjectBD/trainingCompany-repo/src/main/java/pl/trainingCompany/repo/TopicRepo.java
@@ -6,6 +6,6 @@ import pl.trainingCompany.model.dbo.Topic;
 /**
  * Created by Pawel on 24-Mar-16.
  */
-public interface TopicRepo extends CrudRepository<Topic,Long > {
-
+public interface TopicRepo extends CrudRepository<Topic, Long> {
+    public Topic findByName(String name);
 }
