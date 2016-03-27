b3bea5b8544ec2010b42ea9f3b456ed45a746141
pmx1021 <pmx10@wp.pl>
2016-03-27 23:24:34 +0200

    Dodana klasa PostInTopic w dbo

---
+++ ProjectBD/trainingCompany-model/src/main/java/pl/trainingCompany/model/dbo/PostInTopic.java
@@ -0,0 +1,55 @@
+package pl.trainingCompany.model.dbo;
+
+import javax.persistence.*;
+import java.util.Date;
+
+/**
+ * Created by PM on 27.03.2016.
+ * Trzeba zmienic basket!!!!!!!!!!!
+ */
+@Entity
+public class PostInTopic {
+
+    @Id
+    @GeneratedValue
+    private long id;
+
+    @ManyToOne
+    @JoinColumn(name = "topic_id")
+    private Topic topic;
+
+    private Date date;
+    private String content;
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
+
+    public Date getDate() {
+        return date;
+    }
+
+    public void setDate(Date date) {
+        this.date = date;
+    }
+
+    public Topic getTopic() {
+        return topic;
+    }
+
+    public void setTopic(Topic topic) {
+        this.topic = topic;
+    }
+}
