283904184f759919e3c8309034ffbad655dcb122
Paweł <pw.0201@gmail.com>
2016-04-20 19:23:07 +0200

    przekierowanie z wyszukiwarki na głównej

--- ProjectBD/trainingCompany-web/src/main/resources/js/indexControllers.js
+++ ProjectBD/trainingCompany-web/src/main/resources/js/indexControllers.js
@@ -1,30 +1,34 @@
 var indexControllers = angular.module(
     'IndexControllers', []);
 
-indexControllers.controller('RegistrationController',['$scope','IndexService', function($scope, IndexService) {
+indexControllers.controller('RegistrationController', ['$scope', 'IndexService', function ($scope, IndexService) {
     $scope.message = "Registration form";
-    $scope.myUser={};
-    $scope.send = function(){
-        IndexService.postGret({id:$scope.gret.id},$scope.gret);
-    }
+    $scope.myUser = {};
+    $scope.send = function () {
+        IndexService.postGret({id: $scope.gret.id}, $scope.gret);
+    };
     $scope.gret = {};
 
-    IndexService.postGret({id:3}, $scope.gret);
+    IndexService.postGret({id: 3}, $scope.gret);
 }]);
 
-indexControllers.controller('MainPageController',['$scope','IndexService', function($scope,IndexService) {
+indexControllers.controller('MainPageController', ['$scope', 'IndexService', function ($scope, IndexService) {
     $scope.message = "Main page";
     $scope.user;
 
-
-    IndexService.getUser({id:12},function(user){
-        $scope.user =user;
+    IndexService.getUser({id: 12}, function (user) {
+        $scope.user = user;
     });
+
+    $scope.searchPhrase = function () {
+        window.location = "../#/search/" + $scope.phrase;
+    };
+
 }]);
 
-indexControllers.controller('NewController',['$scope','IndexService', function($scope,IndexService) {
+indexControllers.controller('NewController', ['$scope', 'IndexService', function ($scope, IndexService) {
 
-    $scope.inst =IndexService.getGret({id: 2});
+    $scope.inst = IndexService.getGret({id: 2});
 
 
 }]);
