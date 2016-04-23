7bfc6d09dd826cc56393863a281400da4f12eca0
Paweł <pw.0201@gmail.com>
2016-04-23 11:22:14 +0200

    dopieszczona strona główna

--- ProjectBD/trainingCompany-web/src/main/resources/js/public/indexControllers.js
+++ ProjectBD/trainingCompany-web/src/main/resources/js/public/indexControllers.js
@@ -1,22 +1,22 @@
 var indexControllers = angular.module(
     'IndexControllers', []);
 
-
-
 indexControllers.controller('MainPageController', ['$scope', 'IndexService', function ($scope, IndexService) {
     $scope.message = "Main page";
 
-
-    $scope.searchPhrase = function () {
-        window.location = "../#/search/1?query=" + $scope.phrase;
-    }
+    $scope.searchPhrase = function (phrase) {
+        if (typeof phrase == 'undefined') {
+            window.location = "../#/search/1";
+        } else window.location = "../#/search/1?query=" + phrase;
+    };
+    
 }]);
 
 indexControllers.controller('LoginController', ['$scope', 'IndexService', function ($scope, IndexService) {
     $scope.message = "Login page";
 
-    $scope.login = function(){
-        IndexService.login($scope.user,function(data){
+    $scope.login = function () {
+        IndexService.login($scope.user, function (data) {
 
             window.alert('succes');
         });
